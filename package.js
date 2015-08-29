5119bcceb1c41fd5c2f02d95fc06e0d20d8b17b0
Julian Ćwirko <julian.cwirko@gmail.com>
2015-08-29 16:48:49 +0200

    updates

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     'summary': 'Flip effect for s-alert - simple and fancy notifications / alerts for Meteor.',
-    'version': '1.1.4',
+    'version': '3.0.0',
     'git': 'https://github.com/juliancwirko/meteor-s-alert-flip.git',
     'name': 'juliancwirko:s-alert-flip'
 });
 
 Package.onUse(function (api) {
-    api.use('juliancwirko:s-alert@2.4.2', ['client']);
-    api.imply('juliancwirko:s-alert@2.4.2', ['client']);
+    api.use('juliancwirko:s-alert@3.0.0', ['client']);
+    api.imply('juliancwirko:s-alert@3.0.0', ['client']);
     api.addFiles([
         's-alert-flip.css'
     ], 'client');
